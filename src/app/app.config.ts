1b9964198ef28f984c0893f284fcb3287b6696df
mariomo16 <moralesortegamario02@gmail.com>
2026-03-09 18:23:58 +0100

    Optimizations for zone-card component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
+import { registerLocaleData } from "@angular/common";
 import { provideHttpClient, withFetch } from "@angular/common/http";
+import localeEs from "@angular/common/locales/es";
 import {
-	ApplicationConfig,
+	type ApplicationConfig,
+	LOCALE_ID,
 	provideBrowserGlobalErrorListeners,
 	provideZonelessChangeDetection,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 
+registerLocaleData(localeEs);
+
 export const appConfig: ApplicationConfig = {
 	providers: [
+		{ provide: LOCALE_ID, useValue: "es" },
 		provideBrowserGlobalErrorListeners(),
 		provideHttpClient(withFetch()),
 		provideRouter(routes),
--- src/app/components/zone-card/zone-card.ts
+++ src/app/components/zone-card/zone-card.ts
@@ -1,10 +1,11 @@
+import { CurrencyPipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import type { Zone } from "../../core/models/zone.model";
 import { SafeHtmlPipe } from "../../shared/pipes/safe-html.pipe";
 
 @Component({
 	selector: "app-zone-card",
-	imports: [SafeHtmlPipe],
+	imports: [SafeHtmlPipe, CurrencyPipe],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: "./zone-card.html",
 })
