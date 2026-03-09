48209efb9bcd2825d14ac36f9984f846e6f6a797
mariomo16 <moralesortegamario02@gmail.com>
2026-03-09 18:10:41 +0100

    Fox navbar links

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 import { APP } from "../../../core/constants/app.constants";
 
 @Component({
 	selector: "app-navbar",
-	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: "./navbar.html",
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [RouterLink],
 })
 export class Navbar {
 	readonly appName = APP.name;
