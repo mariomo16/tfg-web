2d5d660275feeffeb43449639f1acda7e62d8f02
mariomo16 <moralesortegamario02@gmail.com>
2026-03-10 18:07:13 +0100

    que asco los merge conflicts

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,13 @@ import { RouterOutlet } from "@angular/router";
 })
 export class App {
 	protected readonly title = signal("tfg-web");
+
+	ngOnInit() {
+		// Para que eliminar el FOUC en producción (a la cuarta va la vencida)
+		const splash = document.getElementById("app-splash");
+		const html = document.documentElement;
+
+		html.classList.remove("is-loading");
+		splash?.remove();
+	}
 }
