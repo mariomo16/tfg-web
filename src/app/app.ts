b9703c451079f02b343ca9582d2a6b8a38762ba5
mariomo16 <moralesortegamario02@gmail.com>
2026-03-10 17:38:25 +0100

    FOUC fix ahora si

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject, OnInit, Renderer2, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
@@ -7,6 +7,12 @@ import { RouterOutlet } from "@angular/router";
 	templateUrl: "./app.html",
 	styleUrl: "./app.css",
 })
-export class App {
+export class App implements OnInit {
 	protected readonly title = signal("tfg-web");
+	private readonly renderer = inject(Renderer2);
+
+	ngOnInit() {
+		this.renderer.setStyle(document.body, "visibility", "visible");
+		this.renderer.setStyle(document.body, "opacity", "1");
+	}
 }
