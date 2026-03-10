84869604ced0e17334e7a8574cfc494e8170db43
mariomo16 <moralesortegamario02@gmail.com>
2026-03-10 18:29:13 +0100

    Remove some animations and added button link to /admin

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { APP } from "../../../core/constants/app.constants";
+import { Icons } from "../../../shared/icons/icons";
+import { SafeHtmlPipe } from "../../../shared/pipes/safe-html.pipe";
 
 @Component({
 	selector: "app-navbar",
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [RouterLink],
+	imports: [RouterLink, SafeHtmlPipe],
 	templateUrl: "./navbar.html",
 })
 export class Navbar {
 	readonly appName = APP.name;
+	protected readonly settingsIcon = Icons.settings;
 }
