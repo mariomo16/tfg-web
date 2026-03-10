069c4321f91379c4c8c6a0c36127897e2fd02fad
mariomo16 <moralesortegamario02@gmail.com>
2026-03-10 17:52:45 +0100

    FOUC 3 - 1 Mario, ahora si

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, Renderer2, signal } from "@angular/core";
+import { Component, type OnInit, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
@@ -9,10 +9,24 @@ import { RouterOutlet } from "@angular/router";
 })
 export class App implements OnInit {
 	protected readonly title = signal("tfg-web");
-	private readonly renderer = inject(Renderer2);
 
 	ngOnInit() {
-		this.renderer.setStyle(document.body, "visibility", "visible");
-		this.renderer.setStyle(document.body, "opacity", "1");
+		// Timeout que se ejecutara cuando todo este cargado para que no haya FOUC (a la cuarta va la vencida)
+		setTimeout(() => {
+			const splash = document.getElementById("app-splash");
+			const html = document.documentElement;
+
+			if (splash) {
+				splash.style.opacity = "0";
+				splash.style.visibility = "hidden";
+			}
+
+			html.classList.remove("is-loading");
+
+			// Limpiar el splash del dom despues de la transicion
+			setTimeout(() => {
+				splash?.remove();
+			}, 500);
+		}, 0);
 	}
 }
